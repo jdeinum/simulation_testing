484ad4efc2aeeed7dfe6ed04f5bd6ca322587d93
Jacob Deinum <jdeinum@nullspaces.org>
2025-07-08 09:03:47 -0600

    man some of dis complex >.<

--- src/broadcast.rs
+++ src/broadcast.rs
@@ -1,27 +1,33 @@
 use crate::simulation::Simulation;
-use anyhow::{Context, Result};
+use anyhow::Result;
 use bytes::{BufMut, Bytes, BytesMut};
 use futures::future::join_all;
-use std::collections::HashMap;
-use tokio::{io::AsyncWriteExt, net::TcpStream};
+use std::{
+    collections::HashMap,
+    sync::{Arc, Mutex},
+};
+use tokio::{io::AsyncWriteExt, select};
 
 /// The broadcast layer has a few responsibilities:
 /// 1. When the application wants to send a message, it needs to
 pub struct BroadcastLayer<S: Simulation> {
-    pub peers: HashMap<String, TcpStream>,
-    pub buf: BytesMut,
-    pub s: S,
+    pub peers: Vec<String>,
+    pub log: Arc<Mutex<HashMap<usize, Vec<String>>>>,
+    pub current_sequence_num: usize,
+    pub s: Arc<Mutex<S>>,
 }
 
 impl<S: Simulation> BroadcastLayer<S> {
+    // broadcast a message to our peers
+    // NOTE: We have no retry mechanism because we assume the message will get through. This is of
+    // course a silly assumption. We are injecting faults by messing with the order of messages. If
+    // the system stopped procuding events, we can be certain that all messages would arrive
+    // (eventually consistent)
     pub async fn broadcast(&mut self, message: &[u8]) -> Result<()> {
-        self.buf.clear();
-        self.buf.put(message);
-
         let futures = self
             .peers
-            .values_mut()
-            .map(|stream| stream.write_all(&self.buf))
+            .iter()
+            .map(|p| self.s.lock().unwrap().send_message(p, message))
             .collect::<Vec<_>>();
 
         let results = join_all(futures).await;
@@ -33,18 +39,30 @@ impl<S: Simulation> BroadcastLayer<S> {
         }
     }
 
-    pub async fn receive(&mut self) -> Result<Bytes> {
-        // the only time the broadcast layer is allowed to send message A to the application layer
-        // is when we know that we'll never get a message that comes before A in the total order of
-        // this systems messages.
-
-        // In this faulty example, we will consider that the case when:
-        // 1. We have received NUM_NODE messages with sequence number i. i.e if we have 3 nodes,
-        //    and we have received 1-1, 2-1, and 3-1, we know we'll never get another message with
-        //    sequence number 1, therefore it is safe to deliver to the application.
-        // 2. If we have no received a message from node X in 30 seconds, we assume it to be dead,
-        //    and we decreease the number of messages needed for sequence number X by 1. This is
-        //    where we'll be injecting the faults.
+    // just listen for messages on all of the sockets
+    // these have to be cancellation safe so we use a combination of peek and read to get the
+    // message
+    async fn add_messages_to_queue(&mut self) -> Result<()> {
         todo!()
+    }
+
+    // the only time the broadcast layer is allowed to send message A to the application layer
+    // is when we know that we'll never get a message that comes before A in the total order of
+    // this systems messages.
+
+    // In this faulty example, we will consider that the case when:
+    // 1. We have received NUM_NODE messages with sequence number i. i.e if we have 3 nodes,
+    //    and we have received 1-1, 1-2, and 1-3, we know we'll never get another message with
+    //    sequence number 1, therefore it is safe to deliver to the application.
+    // 2. If we have no received a message from node X in 30 seconds, we assume it to be dead,
+    //    and we decreease the number of messages needed for sequence number X by 1. This is
+    //    where we'll be injecting the faults.
+    pub async fn receive(&mut self) -> Result<Bytes> {
+        todo!()
+        // if we have received NUM_NODES messages, we can deliver these messages to the
+        // application
+
+        // if 30 seconds have passed since we have heard from a node, we consider it dead
+        // and the number of nodes we need to receive messages from decreases by 1
     }
 }
--- src/node.rs
+++ src/node.rs
@@ -1,9 +1,7 @@
-use std::{sync::Arc, time::Duration};
-
 use crate::{broadcast::BroadcastLayer, simulation::Simulation};
 use anyhow::{Context, Result};
 use bytes::Bytes;
-use tokio::sync::Mutex;
+use std::time::Duration;
 use tracing::{info, instrument};
 
 pub struct Node<S: Simulation> {
@@ -12,7 +10,7 @@ pub struct Node<S: Simulation> {
     broadcast_layer: BroadcastLayer<S>,
 }
 
-impl<S: Simulation> Node<S> {
+impl<S: Simulation + Send + 'static> Node<S> {
     // create a new node
     pub fn new(id: String, broadcast_layer: BroadcastLayer<S>) -> Self {
         Self {
@@ -33,22 +31,33 @@ impl<S: Simulation> Node<S> {
 
     pub async fn run(mut self) -> Result<()> {
         // spawn a simple handler that prints messages received from the broadcast layer
-        tokio::spawn(async || {
-            loop {
-                let s = self
-                    .broadcast_layer
-                    .receive()
-                    .await
-                    .context("receive message from broadcast layer")?;
+        tokio::spawn(async move {
+            if let Err(e) = async {
+                loop {
+                    let s = self
+                        .broadcast_layer
+                        .receive()
+                        .await
+                        .context("receive message from broadcast layer")?;
 
-                self.handle_message(s).context("handle message")?;
+                    self.handle_message(s).context("handle message")?;
+                }
+
+                #[allow(unreachable_code)]
+                Ok::<(), anyhow::Error>(()) // to satisfy the compiler
+            }
+            .await
+            {
+                tracing::error!("Node task failed: {e:?}");
             }
         });
 
         // generate an event every second, and broadcast it
         loop {
+            // we expliceitly use SN-ID here so that we can lexigraxically sort these in a priority
+            // queue
             self.broadcast_layer
-                .broadcast(format!("{}-{}", self.id, self.sequence_number).as_bytes())
+                .broadcast(format!("{}-{}", self.sequence_number, self.id).as_bytes())
                 .await
                 .context("broadcast message")?;
             self.sequence_number += 1;
